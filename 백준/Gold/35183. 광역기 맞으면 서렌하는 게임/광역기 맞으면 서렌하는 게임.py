37cbcbaa3832893e18f9a44d401c534d4f7a9a0f
Yoonsoo Chang <ysc9606@gmail.com>
2026-02-04 21:04:07 +0900

    [Gold V] Title: 광역기 맞으면 서렌하는 게임, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/35183. 광역기 맞으면 서렌하는 게임/광역기 맞으면 서렌하는 게임.py
+++ 백준/Gold/35183. 광역기 맞으면 서렌하는 게임/광역기 맞으면 서렌하는 게임.py
@@ -1,45 +1,47 @@
-N = int(input())
-LIVES = 2
-memo = [[LIVES] * 1001 for _ in range(N)]
+import sys
+readline = sys.stdin.readline
 
-LEFT_LIMIT = 1
-RIGHT_LIMIT = 1000
-isSurrendered = False
-for i in range(0, N): # Turn 하나씩 순서대로 수행
-    [L, R] = list(map(int, input().split()))
-    for x, life in enumerate(memo[i]):      
-        if x==0:
-            memo[i][x] = 0
-            continue      
+def solve():
+    N = int(input())
+    origin_range = [*map(int, readline().split())]
+    lost_1_life_range = [[0, 1001]]
 
-        leftCanSurvive =  0
-        rightCanSurvive =  0
-        notMoveSurvive =  0
-
-        # hasLeft
-        if x-1 >= LEFT_LIMIT:
-            leftCanSurvive = memo[i][x] if i==0 else memo[i-1][x-1]
-
-        # hasRight
-        if x+1 <= RIGHT_LIMIT:
-            rightCanSurvive = memo[i][x] if i==0 else memo[i-1][x+1]
-        
-        # not move
-        notMoveSurvive = memo[i][x] if i==0 else memo[i-1][x]
-
-        realLife = max(leftCanSurvive, rightCanSurvive, notMoveSurvive)
-        if realLife == 0:
-            memo[i][x]= 0
-            continue
-
-        canEvade = (x >= L and x <= R)
-        if not canEvade:
-            memo[i][x] = realLife - 1
+    for i in range(1, N):
+        L, R = map(int, readline().split())
+        if origin_range: # has elements
+            oL, oR = origin_range
+            nextL = max(oL - 1, L)
+            nextR = min(oR + 1, R)
+            
+            updated_origin = [nextL, nextR] if nextL <= nextR else None
         else:
-            memo[i][x] = realLife
-
-    if sum(memo[i]) == 0:
-        isSurrendered = True
-        break
+            updated_origin = None
         
-print("Surrender" if isSurrendered else "World Champion")
+        survivors = []
+        for oL, oR in lost_1_life_range:
+            nextL = max(oL - 1, L)
+            nextR = min(oR + 1, R)
+            if nextL <= nextR:
+                survivors.append([nextL, nextR])
+
+        if origin_range:
+            survivors.append([origin_range[0] - 1, origin_range[1] + 1]) 
+        
+        if not survivors:
+            print("Surrender")
+            return
+        
+        survivors.sort()
+        updated_1_lost = [survivors[0]]
+        for i in range(1, len(survivors)):
+            L, R = survivors[i]
+            if updated_1_lost[-1][1] + 1 >= L:
+                updated_1_lost[-1][1] = max(updated_1_lost[-1][1], R)
+            else:
+                updated_1_lost.append([L, R])
+
+        origin_range, lost_1_life_range = updated_origin, updated_1_lost
+
+    print("World Champion")
+ 
+solve()   
