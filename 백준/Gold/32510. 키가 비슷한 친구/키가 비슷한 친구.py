b82e8e825868766695a1407404923710136a72f8
Yoonsoo Chang <ysc9606@gmail.com>
2026-02-08 02:57:14 +0900

    [Gold II] Title: 키가 비슷한 친구, Time: 1760 ms, Memory: 85104 KB -BaekjoonHub

--- 백준/Gold/32510. 키가 비슷한 친구/키가 비슷한 친구.py
+++ 백준/Gold/32510. 키가 비슷한 친구/키가 비슷한 친구.py
@@ -1,60 +1,41 @@
 import sys
+from collections import deque, defaultdict
 input = sys.stdin.readline
 
-MAX_H = 500001
-INF = 10**9
-
 def main():
-    tree = [INF] * (2 * MAX_H)
-    modified = []
-
     T = int(input())
     out = []
 
     for t in range(1, T + 1):
-        # reset
-        for idx in modified:
-            tree[idx] = INF
-        modified = []
-
         N, K = map(int, input().split())
         A = list(map(int, input().split()))
 
+        # 1단계: 각 키의 최소 인덱스 저장
+        mins = defaultdict(lambda: 10**9)
+        for i in range(N):
+            if i < mins[A[i]]:
+                mins[A[i]] = i
+
+        # 2단계: 모노토닉 덱으로 각 키별 결과 계산
+        results = {}
+        q = deque()
+        for h in sorted(A):
+            # 범위 벗어난 키 제거 (h - K 미만)
+            while q and q[0] < h - K:
+                q.popleft()
+
+            # 덱 뒤에서 현재보다 인덱스 큰 것 제거
+            # (인덱스 큰 키는 앞으로 절대 최적해가 될 수 없음)
+            while q and mins[q[-1]] > mins[h]:
+                q.pop()
+
+            q.append(h)
+            results[h] = mins[q[0]]  # 범위 내 최소 인덱스
+
+        # 3단계: 결과 합산
         ans = 0
         for i in range(N):
-            h = A[i]
-            lo = h - K if h >= K else 0
-
-            # inline query
-            res = INF
-            l = lo + MAX_H
-            r = h + MAX_H + 1
-            while l < r:
-                if l & 1:
-                    if tree[l] < res:
-                        res = tree[l]
-                    l += 1
-                if r & 1:
-                    r -= 1
-                    if tree[r] < res:
-                        res = tree[r]
-                l >>= 1
-                r >>= 1
-
-            if res <= i:
-                ans += i - res
-
-            # inline update
-            p = h + MAX_H
-            modified.append(p)
-            if tree[p] > i:
-                tree[p] = i
-                while p > 1:
-                    p >>= 1
-                    modified.append(p)
-                    left = tree[p << 1]
-                    right = tree[(p << 1) | 1]
-                    tree[p] = left if left < right else right
+            ans += i - results[A[i]]
 
         out.append(f"Case #{t}\n{ans}")
 
